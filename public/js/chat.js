55ea30f1d37b6ece324273cac8600d09632574f5
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2021-05-15 15:04:10 +0200

    Add socket connection

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,28 @@
-io()
+document.addEventListener('DOMContentLoaded', () => {
+    const socket = io()
+
+    socket.on('message', (message) => {
+        console.log(message)
+    })
+
+    const $messageForm = document.querySelector('#messageForm')
+    const $input = document.querySelector('#message')
+    $input.value = ''
+
+    $messageForm.addEventListener('submit', (evt) => {
+        evt.preventDefault()
+
+        const regex = /<\/?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->/img
+        const message = $input.value.replace(regex, "")
+
+        if (!message) {
+            $input.value = ''
+            $input.focus()
+            return
+        }
+
+        socket.emit('sendMessage', message)
+
+        $input.value = ''
+    })
+})
--- src/index.js
+++ src/index.js
@@ -17,8 +17,17 @@ app.get('', (req, res) => {
     res.render('index')
 })
 
-io.on('connection', () => {
-    console.log('Socket.IO connected')
+io.on('connection', (socket) => {
+    socket.emit('message', 'Hello, world! I mean, hello there!')
+
+    socket.on('sendMessage', (msg) => {
+        const regex = /<\/?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->/img
+        const message = msg.replace(regex, "")
+
+        if (message) {
+            io.emit('message', message)
+        }
+    })
 })
 
 server.listen(PORT, () => {

d73f7062905fe99bc71b0f36359dec4f85da25ef
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2021-05-15 10:53:58 +0200

    Setup basic socketIO

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
+const http = require('http')
 const path = require('path')
 const express = require('express')
+const socketio = require('socket.io')
 
 const PORT = process.env.PORT
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const publicPath = path.join(__dirname, '../public/')
 app.use(express.static(publicPath))
@@ -13,6 +17,10 @@ app.get('', (req, res) => {
     res.render('index')
 })
 
-app.listen(PORT, () => {
+io.on('connection', () => {
+    console.log('Socket.IO connected')
+})
+
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })

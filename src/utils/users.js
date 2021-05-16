8a8a7bf8de5bf3eb4a7310962fe9e3962d3a8822
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2021-05-16 12:03:36 +0200

    Users utils file

---
+++ src/utils/users.js
@@ -0,0 +1,37 @@
+const users = []
+
+const addUser = ({ id, room, username }) => {
+    username = username.trim()
+    room = room.trim()
+
+    if (!username || !room) return new Error('Username and room are required')
+
+    const existingUser = users.find(user => user.room === room && user.username === username)
+
+    if (existingUser) return new Error('That username is already taken')
+
+    const user = { id, username, room }
+
+    users.push(user)
+
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex(user => user.id === id)
+
+    if (index > -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+const getUser = (id) => users.find(user => user.id === id)
+
+const getUsersInRoom = (room) => users.filter(user => user.room === room)
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
